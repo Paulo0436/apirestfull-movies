156e46b537c7b0833c33ac679906118e1c27cca6
Paulo Andre <p.souz436@gmail.com>
2025-11-26 11:06:10 -0300

    Estruturando o app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,46 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const conectarAoBancoDeDados = require('./config/database');
 
-var app = express();
+// Rotas da documentação
+const documentacaoSwaggerRouter = require('./routes/documentacaoSwaggerRouter');
 
+// Rotas do domínio MOVIES (filmes)
+const categoriaFilmeRouter = require('./routes/categoriaFilmeRouter');
+const duracaoFilmeRouter = require('./routes/duracaoFilmeRouter');
+const estoqueFilmeRouter = require('./routes/estoqueFilmeRouter');
+const diretorFilmeRouter = require('./routes/diretorFilmeRouter');
+const filmeRouter = require('./routes/filmeRouter');
+
+// Rota de usuário (auth)
+const usuarioRouter = require('./routes/usuarioRouter');
+
+const app = express();
+
+// Conexão com o banco
+conectarAoBancoDeDados();
+
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Swagger
+app.use('/api/v1/api-documentation', documentacaoSwaggerRouter);
+
+// Rotas relacionadas a FILMES
+app.use('/api/v1/movie/categoria', categoriaFilmeRouter);
+app.use('/api/v1/movie/duracao', duracaoFilmeRouter);
+app.use('/api/v1/movie/estoque', estoqueFilmeRouter);
+app.use('/api/v1/movie/diretor', diretorFilmeRouter);
+
+app.use('/api/v1/movie', filmeRouter);
+
+// Usuários
+app.use('/api/v1/usuario', usuarioRouter);
 
 module.exports = app;
