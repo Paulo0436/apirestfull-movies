278bf4c14819d988f7fdc39bde2f8a9be486fed2
Paulo Andre <p.souz436@gmail.com>
2025-11-26 11:14:21 -0300

    Estruturando estoque  de filme

---
+++ src/routes/estoqueFilmeRouter.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const estoqueFilmeController = require('../controller/estoqueFilmeController');
+const router = express.Router();
+const autentificacao = require('../middleware/autentificacaoMiddleware');
+
+// Adicionar estoque de filme
+router.post(
+    '/',
+    autentificacao.verificarTokenDeAutentificacao,
+    estoqueFilmeController.adicionarEstoque
+);
+
+// Editar estoque de filme
+router.put(
+    '/:id',
+    autentificacao.verificarTokenDeAutentificacao,
+    estoqueFilmeController.buscarEstoques,
+    estoqueFilmeController.editarEstoque
+);
+
+// Listar todo o estoque de filmes
+router.get(
+    '/',
+    estoqueFilmeController.listarEstoque
+);
+
+// Exibir um estoque específico
+router.get(
+    '/:id',
+    estoqueFilmeController.buscarEstoques,
+    estoqueFilmeController.exibirEstoque
+);
+
+// Deletar estoque de filme
+router.delete(
+    '/:id',
+    autentificacao.verificarTokenDeAutentificacao,
+    estoqueFilmeController.buscarEstoques,
+    estoqueFilmeController.deletarEstoque
+);
+
+module.exports = router;
